4c5ce439a567b554e8fdba9eca82bcbba5fa2fe4
TakuyaMurakami <takuya.murakami.job@outlook.jp>
2024-09-05 15:13:55 +0900

    Initial Commit

---
+++ training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  training
+//
+//  Created by 村上拓也 on 2024/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
