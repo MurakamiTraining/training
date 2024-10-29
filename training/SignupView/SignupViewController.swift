8353a601460529b0a858472e5c9eb117d3fe10cd
MurakamiTraining <takuya.murakami.job@outlook.jp>
2024-10-29 13:17:51 +0900

    ユーザー登録画面を実装
    ・ユーザー登録画面のViewを作成
    ・ID、パスワードを入力して保存
    ・登録完了後、RSS選択画面へ
    ・各種エラー表示を追加

--- training/SignupView/SignupViewController.swift
+++ training/SignupView/SignupViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class SignupViewController: UIViewController {
+    // UIデータのアウトレット接続
+    @IBOutlet weak var userIdInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var cautionLabel: UILabel!
+    
+    // ユーザーId
+    var userId: String = ""
+    // パスワード
+    var password: String = ""
+    
     // 画面読み込み後
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    // 戻るボタン押下後
-    @IBAction func onTapBackButton() {
+    // 新規ユーザー登録ボタン押下後
+    @IBAction func onTapSignupButton() {
+        userIdInput.endEditing(true)
+        passwordInput.endEditing(true)
+        // エラー処理
+        if userId == "" {
+            cautionLabel.text = "ユーザーIDを入力してください"
+            return
+        } else if password == "" {
+            cautionLabel.text = "パスワードを入力してください"
+            return
+        }
+        // ID、パスワードを保存しメインのシーンへ
+        UserDefaults.standard.set(userId, forKey: ProjectConstant.userIdKey)
+        UserDefaults.standard.set(password, forKey: ProjectConstant.passwordKey)
+        navigationController?.moveNextView(storyboardID: .RSSSelect)
+    }
+    
+    // ユーザーID入力後
+    @IBAction func onEditingEndUserId() {
+        guard let userId = userIdInput.text else { return }
+        self.userId = userId
+    }
+    
+    // パスワード入力後
+    @IBAction func onEditingEndPassword() {
+        guard let password = passwordInput.text else { return }
+        self.password = password
     }
 }
